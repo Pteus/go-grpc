00a783fcb71410fd4d5c26e3997273856494c7ed
Pedro Mateus <pedropmateus91@gmail.com>
2024-04-20 22:48:08 +0100

    Fix missing files in commit

---
+++ server/main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"context"
+	"log"
+	"net"
+
+	"example.com/grpcalc/pb"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/reflection"
+	"google.golang.org/grpc/status"
+)
+
+type server struct {
+	pb.UnimplementedCalculatorServer
+}
+
+func (s *server) Add(ctx context.Context, in *pb.CalculationRequest) (*pb.CalculationResponse, error) {
+	return &pb.CalculationResponse{
+		Result: in.A + in.B,
+	}, nil
+}
+
+func (s *server) Divide(ctx context.Context, in *pb.CalculationRequest) (*pb.CalculationResponse, error) {
+	if in.B == 0 {
+		return nil, status.Error(codes.InvalidArgument, "Cannot divide by 0")
+	}
+
+	return &pb.CalculationResponse{
+		Result: in.A / in.B,
+	}, nil
+}
+
+func main() {
+	listener, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln("failed to create listener:", err)
+	}
+
+	s := grpc.NewServer()
+	reflection.Register(s)
+
+	pb.RegisterCalculatorServer(s, &server{})
+	if err := s.Serve(listener); err != nil {
+		log.Fatalln("failed to serve:", err)
+	}
+}
